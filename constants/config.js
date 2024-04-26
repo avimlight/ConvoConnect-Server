0610d6b1f83fde5cd38e8ccbdce0c2ab457ef360
avimlight <avimonteiro08@gmail.com>
2024-04-26 13:23:30 +0530

    modified config.js

--- constants/config.js
+++ constants/config.js
@@ -1,11 +1,12 @@
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    process.env.CLIENT_URL,
+    "http://localhost:5173",    // Local development URL
+    "http://localhost:4173",    // Another possible local development URL
+    "https://convo-connect-client-314ytr0p5-avinashs-projects-1ead76f9.vercel.app", // Specific deployment URL
+    process.env.CLIENT_URL,     // Make sure this environment variable is correctly set
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
+  credentials: true,  // Allow credentials (cookies, authorization headers, etc.) for cross-origin requests
 };
 
 const CHATTU_TOKEN = "chattu-token";
