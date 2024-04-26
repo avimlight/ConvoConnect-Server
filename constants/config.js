94aa4c7dff27015cbb144ea8b0741b2764847a05
avimlight <avimonteiro08@gmail.com>
2024-04-26 15:04:20 +0530

    changes for url

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",    // Local development URL
     "http://localhost:4173",    // Another possible local development URL
-    "https://convo-connect-client-314ytr0p5-avinashs-projects-1ead76f9.vercel.app", // Specific deployment URL
+    "https://convo-connect-client.vercel.app/", // Specific deployment URL
     process.env.CLIENT_URL,     // Make sure this environment variable is correctly set
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
